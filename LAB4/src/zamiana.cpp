8778b3a819189e3e915da62719c94910cf46e0e6
eitipany <maciej.jakub.pawlinski@openmailbox.org>
2017-05-08 07:59:05 +0200

    Ćwiczenie 4
    Wszystkie pliki do ćw 4

---
+++ LAB4/src/zamiana.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+int main()
+{
+	int i;
+	std::cout << "i=";
+	std::cin >> i;
+	std::cout << std::endl;
+	float tab[i][4];
+	for(int k=0;k<i;++k)
+	{
+		std::cout << "wartosci zmiennych dla i=" << k+1 << std::endl;
+		std::cout << "a" << k+1 <<"=";
+		std::cin >> tab[k][0];
+		std::cout << std::endl;
+		std::cout << "d" << k+1 <<"=";
+		std::cin >> tab[k][1];
+		std::cout << std::endl;
+		std::cout << "alfa" << k+1 <<"=";
+		std::cin >> tab[k][2];
+		std::cout << std::endl;
+		std::cout << "teta" << k+1 <<"=";
+		std::cin >> tab[k][3];
+		std::cout << std::endl;
+	}
+	for (int o=0; o<i; ++o)
+	{	
+		float cost = cos(tab[o][3]);
+		float sint = sin(tab[o][3]);
+		float cosa = cos(tab[o][2]);
+		float sina = sin(tab[o][2]);
+
+		float yaw = atan2(cost*sina, cosa);
+		float pitch = atan2(-1*sint*sina, sqrt(1 - cost*sina*cost*sina));
+		float roll = atan2(sint*cosa, cost);
+
+		std::cout << "roll: " << roll << " pitch: " << pitch << " yaw: " << yaw << std::endl;
+	}
+}
