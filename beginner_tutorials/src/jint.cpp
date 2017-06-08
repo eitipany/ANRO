88471e298c285260424e6680cbb24152dc8af465
eitipany <maciej.jakub.pawlinski@openmailbox.org>
2017-06-08 22:48:37 +0200

    Add files via upload

---
+++ beginner_tutorials/src/jint.cpp
@@ -0,0 +1,195 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/JointState.h>
+#include <visualization_msgs/Marker.h>
+#include <math.h>
+#include <kdl/chain.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/frames.hpp>
+#include <csignal>
+#include <nav_msgs/Path.h>
+
+using namespace std;
+using namespace KDL;
+
+volatile int exitFlag = 0;
+ double a2  = 1.0;
+                        double d1 = 0.6;
+                        double a3 = 1.0;
+
+void sigintHandler(int) {
+	exitFlag = 1;
+}
+ 
+//publishTrajectoryPath
+geometry_msgs::PoseStamped poseStamped; 
+int main(int argc, char **argv){
+	ROS_INFO("tu je "); 	
+	double x,y,z,t, xxx, yyy, zzz;
+	double ox, oy, oz; 
+	double dx, dy, dz; 
+	
+	ros::init(argc, argv, "jint");
+	ros::NodeHandle s; 
+	ros::NodeHandle n; 
+	ros::Rate loop_rate(10);
+	ros::Publisher pub = s.advertise<geometry_msgs::PoseStamped>("jint",1);
+		ros::Publisher ukl = s.advertise<geometry_msgs::PoseStamped>("ukl",1000);
+	ros::Publisher joint = s.advertise<sensor_msgs::JointState>("joint_states",1);  
+	ros::Publisher path_pub;
+        path_pub=n.advertise<nav_msgs::Path>("path1",1);
+        nav_msgs::Path path;
+        std::vector<geometry_msgs::PoseStamped> plan;
+
+	double d1,d2,d3;
+
+//	s.param<double>("d1",d1,3);
+ //   	s.param<double>("d2",d2,3);
+//	s.param<double>("d3",d3,3);
+
+	sensor_msgs::JointState robotState; 
+	robotState.name.push_back("part0part1");
+	robotState.name.push_back("part1part2");
+	robotState.name.push_back("part2part3");
+	robotState.position.resize(robotState.name.size());
+	
+
+//	d1 = 3;
+//	d2 = 0;
+//	d3 = 1.5;
+	ox=0; 
+	oy=0; 
+	oz=0; 
+
+	robotState.header.stamp = ros::Time::now(); 	
+	robotState.position[0] = ox;
+	robotState.position[1] = oy;
+	robotState.position[2] = oz;
+	joint.publish(robotState); 
+	ROS_INFO("tu tez je "); 
+	while(ros::ok()|| !exitFlag){
+
+		
+		ROS_INFO("Podaj zadane x y z i czas ");  
+		std::cin>>xxx>>yyy>>zzz>>t;
+				
+		zzz=zzz-0.6;
+		double e,f,c,A,B,G,cosB,cosG;
+        
+    double eps=0.01;
+    
+    double a22=pow(a2,2);
+    double a32=pow(a3,2);
+    
+    double x0=0;
+    double y0=d1;
+    double x1=sqrt(pow(xxx,2)+pow(yyy,2));
+    double y1=d1;
+    double x2=x1;
+    double y2=zzz;
+    
+    c=sqrt(pow(x2-x0,2)+pow(y2-y0,2));
+    e=sqrt(pow(x1-x0,2)+pow(y1-y0,2));
+    f=y2-y1;
+    
+    A=atan(f/e);
+    
+    cosB=(a32-a22-pow(c,2))/(-2*a2*c);
+    B=acos(cosB);
+    
+    cosG=(pow(c,2)-a22-a32)/(-2*a2*a3);
+    G=acos(cosG);
+           
+	x=atan2(yyy,xxx);
+	y=-A-B;
+	z=M_PI-G;
+	
+	
+
+		dx = (x-ox)/(10*t); 
+		dy = (y-oy)/(10*t); 
+		dz = (z-oz)/(10*t); 
+
+		for(int i=0; i<(int)10*t; i++){
+			robotState.header.stamp = ros::Time::now(); 
+		
+			robotState.position[0] +=dx;
+			robotState.position[1] +=dy;
+			robotState.position[2] +=dz;
+			double t1 = robotState.position[0];
+    			double t2 = robotState.position[1];
+			double t3 = robotState.position[2];
+			
+			
+	 		KDL::Chain chain;
+
+        		chain.addSegment(Segment(Joint(Joint::None),Frame(Frame::DH(0, 0, 0, 0))));
+        		chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(0, 0, d1, 0))));
+        		chain.addSegment(Segment(Joint(Joint::None),Frame(Frame::DH(0, -M_PI/2, 0, 0))));
+        		chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(a2, 0, 0, 0))));
+        		chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(a3, 0, 0, 0))));
+			chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(0, 0, 0, 0))));
+                        
+
+        		ChainFkSolverPos_recursive solver(chain);
+        		JntArray q(chain.getNrOfJoints());
+			Frame F;
+			
+			  q(0)=t1;
+                	  q(1)=t2;
+                	  q(2)=t3;
+
+                		solver.JntToCart(q,F);
+
+                		double xx,yy,zz=0;
+
+                		xx = F.p.data[0];
+                		yy = F.p.data[1]; 
+                		zz = F.p.data[2];
+
+                		q(0)=t1/2;
+                		q(1)=t2/2;
+                		q(2)=t3/2;
+				solver.JntToCart(q,F);
+
+			ROS_INFO("aktualne : x=%f, y=%f, z=%f",xx,yy,zz); 			
+			poseStamped.header.stamp = ros::Time::now();
+                        poseStamped.header.frame_id="base_link";
+                        poseStamped.pose.position.x=xx;
+                        poseStamped.pose.position.y=yy;
+                        poseStamped.pose.position.z=zz+0.6; 			
+			ukl.publish(poseStamped); 
+			plan.push_back(poseStamped);
+                        poseStamped.header.stamp = ros::Time::now();
+                        //int j = path.size();
+                        path.poses.resize(plan.size());
+
+                        if(!plan.empty()){
+                                path.header.frame_id = "base_link";
+                                path.header.stamp = plan[0].header.stamp;
+                        }
+
+                        for(int j=0; j < plan.size(); j++){
+                                path.poses[j] = plan[j];
+                        }
+
+
+                        //pub.publish(poseStamped); 
+                        path_pub.publish(path);
+
+
+			joint.publish(robotState); 
+			loop_rate.sleep();
+		}
+		
+		ox = robotState.position[0]; 
+		oy = robotState.position[1];
+		oz = robotState.position[2];
+
+		loop_rate.sleep();
+	}
+	
+	ros::spin();
+
+	return 0;
+}
---
+++ beginner_tutorials/src/kdl_dkin.cpp
@@ -0,0 +1,94 @@
+
+#include "ros/ros.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "sensor_msgs/JointState.h"
+#include <signal.h>
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <kdl/kdl.hpp>
+#include <kdl/tree.hpp>
+#include <kdl/chain.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/frames.hpp>
+
+using namespace KDL;
+
+ros::Publisher pub;
+
+void msgReceived(const sensor_msgs::JointState &jointState){
+	double t1 = jointState.position[0];
+	double t2 = jointState.position[1];
+        double t3 = jointState.position[2];
+	
+	double a2  = 1.0;
+        double d1 = 0.6;
+
+
+	 KDL::Chain chain; // Stworzenie łańcucha kinematycznego
+
+        chain.addSegment(Segment(Joint(Joint::None),Frame(Frame::DH(0, 0, 0, 0))));
+        chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(0, 0, d1, 0))));
+        chain.addSegment(Segment(Joint(Joint::None),Frame(Frame::DH(0, -M_PI/2, 0, 0))));
+        chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(a2, 0, 0, 0))));
+        chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(0, 0, 0, 0))));
+
+        ChainFkSolverPos_recursive solver(chain);
+        JntArray q(chain.getNrOfJoints());
+        Frame F;
+	
+	
+
+
+                q(0)=t1;
+                q(1)=t2;
+                q(2)=t3;
+
+                solver.JntToCart(q,F);
+
+                double x,y,z=0; // Współrzędne końcówki
+
+                x = F.p.data[0];
+                y = F.p.data[1]; 
+                z = F.p.data[2];
+
+                q(0)=t1/2;
+                q(1)=t2/2;
+                q(2)=t3/2;
+                solver.JntToCart(q,F);
+
+
+
+
+
+	geometry_msgs::PoseStamped poseStamped;
+    	poseStamped.header.frame_id="base_link";
+    
+	poseStamped.header.stamp = ros::Time::now();
+  
+	poseStamped.pose.position.x=x;
+        poseStamped.pose.position.y=y;
+        poseStamped.pose.position.z=z;
+
+	 poseStamped.pose.orientation.w = F.M.data[0];
+         poseStamped.pose.orientation.x = F.M.data[4];
+         poseStamped.pose.orientation.y = -F.M.data[1];
+         poseStamped.pose.orientation.z = F.M.data[3];
+	pub.publish(poseStamped);
+
+}
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "KDL_DKIN");
+  ros::NodeHandle n;
+  ros::Rate loop_rate(30);
+  pub=n.advertise<geometry_msgs::PoseStamped>("kdl",1000);
+  ros::Subscriber sub = n.subscribe("/joint_states", 100, msgReceived);
+  
+  ros::spin();
+
+  return 0;
+}
+
